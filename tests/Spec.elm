59ea6678a65d928bb2d7f284a3038603ec0848b6
hadrienmp <github@hadrienmp.fr>
2023-05-07 10:47:55 +0200

    refactor: prepare tests for right move

--- tests/Spec.elm
+++ tests/Spec.elm
@@ -18,20 +18,20 @@ suite =
     describe "2048"
         [ describe "Déplacement vers la gauche"
             [ lineParamTest "Aligne les tuiles sur le bord droit"
-                [ "0002 -> 2000"
-                , "0004 -> 4000"
-                , "2000 -> 2000"
-                , "0020 -> 2000"
-                , "0024 -> 2400"
+                [ "0002 + L = 2000"
+                , "0004 + L = 4000"
+                , "2000 + L = 2000"
+                , "0020 + L = 2000"
+                , "0024 + L = 2400"
                 ]
             , lineParamTest "les tuiles égales s'ajoutent"
-                [ "0022 -> 4000"
-                , "4022 -> 4400"
+                [ "0022 + L = 4000"
+                , "4022 + L = 4400"
                 ]
             , lineParamTest "Des tuiles déjà fusionnées ne peuvent pas fusioner sur le même tour"
-                [ "2222 -> 4400"
-                , "4422 -> 8400"
-                , "2244 -> 4800"
+                [ "2222 + L = 4400"
+                , "4422 + L = 8400"
+                , "2244 + L = 4800"
                 ]
             ]
         ]
@@ -47,7 +47,7 @@ lineParamTest name params =
                         \_ ->
                             let
                                 ( input, expected ) =
-                                    case testName |> String.split " -> " of
+                                    case testName |> String.split " + L = " of
                                         [ a, b ] ->
                                             ( a, b )
 
